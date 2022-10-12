0c9d7eaef5ff14768224985dc6d8653696b5bfeb
inan <inan7978@icloud.com>
2022-10-12 19:29:31 -0400

    Lint added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import "./App.css";
-import Home from "./views/Home";
-import SignIn from "./views/SignIn";
-import SignUp from "./views/SignUp";
-import Forgot from "./views/Forgot";
-import Reset from "./views/Reset";
-import Listings from "./views/Listings";
-import CreateListing from "./views/CreateListing";
-import EditListing from "./views/EditListing";
-import EditProfile from "./views/EditProfile";
-import Dashboard from "./views/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import './App.css';
+import Home from './views/Home';
+import SignIn from './views/SignIn';
+import SignUp from './views/SignUp';
+import Forgot from './views/Forgot';
+import Reset from './views/Reset';
+import Listings from './views/Listings';
+import CreateListing from './views/CreateListing';
+import EditListing from './views/EditListing';
+import EditProfile from './views/EditProfile';
+import Dashboard from './views/Dashboard';
 
 function App() {
   return (
     <Layout>
       <Router>
         <Routes>
-          <Route path="/sign-in" exact element={<SignIn />} />
-          <Route path="/sign-up" exact element={<SignUp />} />
-          <Route path="/forgot" exact element={<Forgot />} />
-          <Route path="/reset" exact element={<Reset />} />
-          <Route path="/listings" exact element={<Listings />} />
-          <Route path="/create-listing" exact element={<CreateListing />} />
-          <Route path="/edit-listing" exact element={<EditListing />} />
-          <Route path="/edit-profile" exact element={<EditProfile />} />
-          <Route path="/dashboard" exact element={<Dashboard />} />
-          <Route path="/" exact element={<Home />} />
+          <Route path='/sign-in' exact element={<SignIn />} />
+          <Route path='/sign-up' exact element={<SignUp />} />
+          <Route path='/forgot' exact element={<Forgot />} />
+          <Route path='/reset' exact element={<Reset />} />
+          <Route path='/listings' exact element={<Listings />} />
+          <Route path='/create-listing' exact element={<CreateListing />} />
+          <Route path='/edit-listing' exact element={<EditListing />} />
+          <Route path='/edit-profile' exact element={<EditProfile />} />
+          <Route path='/dashboard' exact element={<Dashboard />} />
+          <Route path='/' exact element={<Home />} />
         </Routes>
       </Router>
     </Layout>
